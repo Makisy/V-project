5f0ef704ccf0db38aef9894ff41d4503ff014aa1
Makisy <twinxiyo@gmail.com>
2023-07-08 00:44:53 +0200

    Add some scripts for dialouge and change some alingment issues

---
+++ V-ProjectUnity/Assets/_Main/Scripts/Core/DialougeContainer.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialougeContainer
+{
+    public GameObject root;
+    public TMP_Text nameText;
+    public TMP_Text dialogueText;
+
+}
